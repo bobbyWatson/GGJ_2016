3ff04d1c506e1457412a70dc01a6687e66335971
BobbyWatson <louis.arthur.cabrera@gmail.com>
2016-01-30 11:15:07 +0100

    player moving

--- Assets/scripts/persos/PlayerLouis.cs
+++ Assets/scripts/persos/PlayerLouis.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public partial class Player : MonoBehaviour {
 
-	public string louis;
+	private Rigidbody2D mRigidBody;
+	public int speed;
 
 	void AwakeLouis(){
-
+		mRigidBody = GetComponent<Rigidbody2D> ();
 	}
 
 	void StartLouis(){
@@ -14,7 +16,8 @@ public partial class Player : MonoBehaviour {
 	}
 
 	void FixedUpdateLouis(){
-
+		Vector2 force = new Vector2( Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
+		mRigidBody.AddForce(force);
 	}
 
 	void UpdateLouis(){
