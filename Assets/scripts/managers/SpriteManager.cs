364e57d3f1f5e16059772ce01b0a8fa3438e999e
BobbyWatson <louis.arthur.cabrera@gmail.com>
2016-01-30 12:14:11 +0100

    push sprite order in progress

--- Assets/scripts/managers/SpriteManager.cs
+++ Assets/scripts/managers/SpriteManager.cs
@@ -13,21 +13,46 @@ public class SpriteManager : MonoBehaviour {
 		}
 	}
 
-	private List<Transform> sprites;
+	private List<Transform> meubles;
+	private List<Transform> props;
 
 	void Start(){
 		GetGoodSprites ();
 	}
 
 	void GetGoodSprites(){
-		sprites = new List<Transform> ();
-		GameObject[] objects = GameObject.FindGameObjectsWithTag ("ChangeIndexSprite");
-		foreach (GameObject o in Object) {
-			sprites.Add (o.transform);
+		meubles = new List<Transform> ();
+		props = new List<Transform> ();
+		GameObject[] objects = GameObject.FindGameObjectsWithTag ("MEUBLE");
+		foreach (GameObject o in objects) {
+			meubles.Add (o.transform);
+		}
+		objects = GameObject.FindGameObjectsWithTag ("PROP");
+		foreach (GameObject o in objects) {
+			props.Add (o.transform);
 		}
 	}
 
 	public void SetSpritesIndex (Rect playerPos){
-
+		foreach (Transform t in meubles) {
+			if (t != null) {
+				float index = 0;
+				if ((t.position + Vector3.down * t.localScale.y / 2).y < (playerPos.position + Vector2.down * playerPos.height /2).y) {
+					t.position = new Vector3 (t.position.x, t.position.y, -1);
+				} else {
+					t.position = new Vector3 (t.position.x, t.position.y, 2);
+				}
+			}
+		}
+		foreach (Transform t in props) {
+			if (t != null) {
+				float index = 0;
+				if ((t.position + Vector3.down * t.localScale.y / 2).y < (playerPos.position + Vector2.down * playerPos.height /2).y) {	
+					t.position = new Vector3 (t.position.x, t.position.y, -2);
+				} else {
+					t.position = new Vector3 (t.position.x, t.position.y, 1);
+				}
+			}
+		}
 	}
 }
