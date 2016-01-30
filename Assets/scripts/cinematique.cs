f5fc157ce2d2c7e7ff42f61d9dcd00b3ec7c0878
jacqueslelezard <jacques.le.lezard@gmail.com>
2016-01-30 14:32:46 +0100

    Now allow several intro cinematics

--- Assets/scripts/cinematique.cs
+++ Assets/scripts/cinematique.cs
@@ -4,29 +4,32 @@ using UnityEngine.SceneManagement;
 
 public class cinematique : MonoBehaviour {
 
-	public GameObject[] cinematique_front;
-	public GameObject[] cinematique_back;
-	public GameObject[] cinematique_decor_back;
 	public GameObject[] cinematique_bloc;
+	private GameObject current_scene;
 	private int i = 0;
+	private int nb_screen = 1;
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log (i);
 		StartCoroutine(SleepSecs(3));
-		Instantiate(cinematique_bloc[0], new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+		current_scene = (GameObject) Instantiate(cinematique_bloc[i], new Vector3(0, 1, 20), Quaternion.identity);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		cinematique_front[i].transform.Translate (0.4f*Time.deltaTime, 0, 0);
-		cinematique_back[i].transform.Translate (-0.2f*Time.deltaTime, 0, 0);
-		cinematique_decor_back[i].transform.Translate (-0.1f*Time.deltaTime, 0, 0);
+
 	}
 
 
 	IEnumerator SleepSecs (float secs) {
 		yield return new WaitForSeconds(secs);
-		SceneManager.LoadScene("Main");
+		StartCoroutine(SleepSecs(3));
+		if (i == nb_screen) {
+			SceneManager.LoadScene ("Main");
+		} else {
+			Destroy(current_scene);
+			i++;
+			current_scene = (GameObject) Instantiate(cinematique_bloc[i], new Vector3(0, 1, 20), Quaternion.identity);
+		}
 	}
 }
