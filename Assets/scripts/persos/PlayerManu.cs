11d6637ecbae5d1be14619e3bebe03a86366536b
Emmanuel Charon <emmanuelcharon@MacBook-Pro-de-Emmanuel-2.local>
2016-01-30 13:03:40 +0100

    find grabable objects

--- Assets/scripts/persos/PlayerManu.cs
+++ Assets/scripts/persos/PlayerManu.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public partial class Player : MonoBehaviour {
 
-	public RitualObject ritualObject;
-	public RitualSpot ritualSpot;
+	public RitualObject ritualObject; // current object in hands
+
+	private static float grabRange = 200.0f;
 
 	void AwakeManu (){
 
@@ -19,13 +20,25 @@ public partial class Player : MonoBehaviour {
 	}
 
 	void UpdateManu(){
-
 		if (Input.GetKeyDown ("down")) {
-
-
-
+			RitualObject rO = this.getGrabableObject ();
+			if (rO != null) {
+				Debug.Log ("Player can grab " + rO.objectName);
+			} else {
+				Debug.Log ("Player cannot grab anything");
+			}
 		}
-			
+	}
 
+	/**
+	 * Returns null if no object is within grabRange
+	 * Returns the closest grabable object
+	 */
+	public RitualObject getGrabableObject() {
+		Transform t = SpriteManager.Instance.GetGrabableObject (this.mTransform.position, grabRange);
+		if (t != null) {
+			return t.gameObject.GetComponent<RitualObject> ();
+		}
+		return null;
 	}
 }
