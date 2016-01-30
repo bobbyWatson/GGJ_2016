19306d458480500a208f0529e01ae274f2db8a9d
bobbyWatson <louis.arthur.cabrera@gmail.com>
2016-01-30 10:29:54 +0100

    modif de la nuit

---
+++ Assets/scripts/dialoguesElements/GroupeParam.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "newGroup", menuName = "Group Config", order = 0)]
+public class GroupeParam : ScriptableObject {
+
+    [SerializeField]
+    public Conversation[] conversations;
+	
+}
---
+++ Assets/scripts/editor/CreateGroupInstance.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class CreateGroupInstance : MonoBehaviour {
+
+	public void Create()
+    {
+
+
+    }
+}
