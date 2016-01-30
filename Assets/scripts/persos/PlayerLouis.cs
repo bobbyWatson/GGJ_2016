7bedb810bcde278295691bc2be7c05890683a7b8
BobbyWatson <louis.arthur.cabrera@gmail.com>
2016-01-30 11:03:24 +0100

    player base

---
+++ Assets/scripts/persos/PlayerLouis.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public partial class Player : MonoBehaviour {
+
+	public string louis;
+
+	void AwakeLouis(){
+
+	}
+
+	void StartLouis(){
+
+	}
+
+	void FixedUpdateLouis(){
+
+	}
+
+	void UpdateLouis(){
+
+	}
+}
